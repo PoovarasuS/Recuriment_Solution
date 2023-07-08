5106afa95d489bf850ba35e4defebf30654e32d4
Poovarasu S <110767446+PoovarasuS@users.noreply.github.com>
2023-07-08 19:57:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function searchCandidates() {
+    const searchBar = document.getElementById('searchBar').value;
+    const location = document.getElementById('location').value;
+    const role = document.getElementById('role').value;
+  }
+  
